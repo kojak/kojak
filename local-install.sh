a2dbb3f3f14e5d548413d2e976bbc64d7708aa11
Salim Badakhchani <sal@redhat.com>
2013-12-20 19:53:48 +0000

    Updated local-install.sh with user execution check

--- local-install.sh
+++ local-install.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
+# 
+# Copyright (C) 2013 Red Hat Inc.
+# Author: Salim Badakhchani <sal@redhat.com>
 #
-# Simple script to automate the installtion of the Koji build system.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+#
+########################################################################
+
+# Uncomment to debug script
+#set -x
+
+# Check to see if user is root
+if [ "$USER" != "root" ]; then
+    echo -e "\n# This script requires root priviledges to run"
+    exit
+fi
 
 # Install Koji dependencies & platform binaries
 yum -y install httpd mod_ssl openssl postgresql-server mod_wsgi yum-utils mock \
