921ff39b9dd25d4577ef6b867cc8001405fe87fb
Salim Badakhchani <sal@redhat.com>
2014-06-23 19:32:20 +0100

    Changed references to ip's to hostnames for portability

--- scripts/install/uninstall.d/002-uninstall-aprox.sh
+++ scripts/install/uninstall.d/002-uninstall-aprox.sh
@@ -7,7 +7,7 @@ echo "Returning fstab to original state..."
 cat /etc/fstab.orig > /etc/fstab
 
 echo "Removing aprox autofs configuration..."
-rm /etc/auto.aprox
+rm -f /etc/auto.aprox
 
 echo "Removing .m2 directory from koji home directory..."
 rm -rf /home/koji/.m2
