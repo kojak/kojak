f6ffb9a5b2ab8569ac3be2a5c97a7cbcca62b37b
jiri.prajzner <jiri.prajzner@ibacz.eu>
2014-12-08 00:08:22 +0100

    added wget for downloading vbox on windows

--- scripts/install/win_vagr_vbox.cmd
+++ scripts/install/win_vagr_vbox.cmd
@@ -1,5 +1,5 @@
 rem ==== We're all born bald, Virtualbox ====
-C:\wget\bin\wget.exe "http://download.virtualbox.org/virtualbox/4.3.18/VirtualBox-4.3.18-96516-Win.exe"
+..\..\wget\bin\wget.exe "http://download.virtualbox.org/virtualbox/4.3.18/VirtualBox-4.3.18-96516-Win.exe"
 VirtualBox-4.3.18-96516-Win.exe -extract
 cd C:\Users\%username%\AppData\Local\Temp\VirtualBox
 msiexec /i VirtualBox-4.3.18-r96516-MultiArch_%processor_architecture%.msi
